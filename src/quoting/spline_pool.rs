2ac3b3e37f80b9ffc73c5cb116586141b4cc17fa
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2025-07-14 13:57:44 -0700

    Adds spline extension module (#7)

---
+++ src/quoting/spline_pool.rs
@@ -0,0 +1,8 @@
+use crate::quoting::base_pool::{
+    BasePool, BasePoolResources, BasePoolState, BasePoolQuoteError
+};
+
+pub type SplinePool = BasePool;
+pub type SplinePoolState = BasePoolState;
+pub type SplinePoolResources = BasePoolResources;
+pub type SplinePoolQuoteError = BasePoolQuoteError;
